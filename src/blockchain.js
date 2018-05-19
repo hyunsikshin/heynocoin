ca40f539580ae4aacfef1ceb6b113a39e5713120
hyunsik <hyunsik.dev@gmail.com>
2018-05-19 22:17:08 +0900

    Creating the Genesis Block

--- src/blockchain.js
+++ src/blockchain.js
@@ -7,3 +7,15 @@ class Block {
     this.data = data;
   }
 }
+
+const genesisBlock = new Block(
+  0,
+  '5BF8F85A1BD8B6C61248E110A1EE140BDB6F68F146A583C781F52BAEC3FA3322',
+  null,
+  1526735534007,
+  'This is the GenesisBlock!'
+);
+
+let blockchain = [genesisBlock];
+
+console.log(blockchain);
