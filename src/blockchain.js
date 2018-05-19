03d6cd67299cf3d8f7b95ecf0b8d05e276469cf1
hyunsik <hyunsik.dev@gmail.com>
2018-05-19 19:17:16 +0900

    Creating the block structure

---
+++ src/blockchain.js
@@ -0,0 +1,9 @@
+class Block {
+  constructor(index, hash, previousHash, timestamp, data) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.data = data;
+  }
+}
